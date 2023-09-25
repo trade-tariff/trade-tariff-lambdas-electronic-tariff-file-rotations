eab6ccbde18087ff01386c680613039b3e211f2e
Amber Cronin <amber@amber.vision>
2023-09-25 15:17:36 +0100

    HOTT-4030: Add initial S3 service code

--- electronic-tariff-file-rotations/go.mod
+++ electronic-tariff-file-rotations/go.mod
@@ -3,3 +3,9 @@ module github.com/trade-tariff/trade-tariff-lambdas-electronic-tariff-file-rotat
 go 1.21.0
 
 require github.com/aws/aws-lambda-go v1.41.0
+
+require (
+	github.com/aws/aws-sdk-go v1.45.15 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- electronic-tariff-file-rotations/main.go
+++ electronic-tariff-file-rotations/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	// "encoding/json"
 	"fmt"
+	"log"
 	"os"
-	// "time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	// "github.com/joho/godotenv"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/joho/godotenv"
 )
+
+var defaultConfig = map[string]string{
+	"ETF_BUCKET": "my-cool-test-bucket-844815912454",
+	"DEBUG":      "false",
+}
 
 type LambdaEvent struct {
 	Date string `json:"date"`
-}
-
-func handler(event *LambdaEvent) {
-	fmt.Println("Hello, world!")
-	return
 }
 
 func main() {
@@ -25,4 +30,58 @@ func main() {
 	} else {
 		handler(nil)
 	}
+}
+
+func initializeEnvironment() {
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load()
+
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	for key, defaultValue := range defaultConfig {
+		if val, exists := os.LookupEnv(key); !exists || val == "" {
+			err := os.Setenv(key, defaultValue)
+
+			if err != nil {
+				log.Fatal("Error occurred while setting environment variable")
+			}
+		}
+	}
+}
+
+func getAWSSession() *session.Session {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-2"),
+	})
+
+	if err != nil {
+		log.Fatal("Error while creating AWS session.")
+	}
+
+	return sess
+}
+
+func handler(event *LambdaEvent) {
+	initializeEnvironment()
+
+	sess := getAWSSession()
+
+	s3svc := s3.New(sess)
+
+	resp, err := s3svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(os.Getenv("ETF_BUCKET"))})
+
+	if err != nil {
+		log.Println("Error getting bucket files.")
+		log.Fatal(err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name: ", *item.Key)
+		fmt.Println("Last modified", item.LastModified.Format("2006-01-02"))
+	}
+
+	return
 }
